8bc3b4c440e4beca0d1e2973816bd2f47d6db6b3
Lasha Kajaia <lashakajaia31@gmail.com>
2022-06-05 16:49:16 +0400

    Changed dashboard styling

--- modules/auth/resources/views/index.blade.php
+++ modules/auth/resources/views/index.blade.php
@@ -1,8 +1,18 @@
 @extends('auth::layouts.app')
 
 @section('content')
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
+<div class="col-md-6">
+    <div class="card shadow-sm border-0 rounded-3">
+        <div class="card-header border-light d-flex align-items-center justify-content-between">
+            <h6 class="m-0">Hey, {{ Auth::user()->name }}!</h6>
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-sm btn-danger px-4 shadow-sm rounded-3">Logout</button>
+            </form>
+        </div>
+        <div class="card-body">
+            You're authenticated!
+        </div>
+    </div>
+</div>
 @endsection
